2eac425747f94b86ecb5eb7a5e755ea4174f95ef
Ashe <fangjia187@163.com>
2018-05-13 23:55:13 +0800

    vue传值练习

---
+++ vueTest/vuetest180513/src/components/InView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="inview">
+    子组件
+    <p>{{sendData.message}}</p>
+    <h4>子传父</h4>
+    <button @click="$emit('son-change','这是儿子抛出的值')">子传父触发按钮</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "in-view",
+    props: ['sendData'],
+    methods: {
+      handleClick() {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .inview {
+    background: lightgray;
+  }
+</style>
---
+++ vueTest/vuetest180513/src/components/outView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    父组件
+    <i>{{number}}</i>
+    <in-view :sendData="sendData" @son-change="getSonChange"></in-view>
+  </div>
+</template>
+
+<script>
+  import InView from './InView'
+
+  export default {
+    name: "out-view",
+    data() {
+      return {
+        sendData:{
+          message:'父组件传过来的信息'
+        },
+        number:1
+      }
+    },
+    methods:{
+      getSonChange(val){
+        console.log(val);
+        console.log('点击子组件了。')
+      }
+    },
+    components: {
+      InView
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
